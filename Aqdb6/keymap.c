c9285601a2ae11bc4b71a3a8ead1eb97ae6f6937
Luis <lmqferreira@live.com>
2025-11-12 22:25:05 +0000

    Refactor process_record_user to call process_record_quantum for improved functionality

--- Aqdb6/keymap.c
+++ Aqdb6/keymap.c
@@ -203,5 +203,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
   }
-  return process_record_user_quantum(keycode, record);
+  return process_record_quantum(keycode, record);
 }
